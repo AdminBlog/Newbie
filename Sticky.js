04b9a77c59bfecf1eee121a39b7f0e03228ee163
TipsRyand <ryandunderscore2@gmail.com>
2017-05-14 23:40:40 +0700

    Create Sticky.js

---
+++ Sticky.js
@@ -0,0 +1,2 @@
+$(function(){if($("#HTML1").length){var o=$("#HTML1"),t=$("#HTML1").offset().top,i=$("#HTML1").height();$(window).scroll(function(){var s=$("#footer-wrapper").offset().top-i-20,f=$(window).scrollTop();if(f>t?o.css({position:"fixed",top:70}):o.css("position","static"),f>s){var n=s-f;o.css({top:n})}})}});
+var postperpage=4;var numshowpage=3;var upPageWord ='Prev';var downPageWord ='Next';var urlactivepage=location.href;var home_page="/";
